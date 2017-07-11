14f87c4dfb9594f27e33688029be9df178fbd524
MrZWH <1875290692@qq.com>
2017-07-11 16:52:13 +0800

    完成所有功能

--- js/base.js
+++ js/base.js
@@ -1,25 +1,145 @@
-; (function () {
+;
+(function () {
     'use strict';
 
-    var $form_add_task = $('.add-task')
-        , $task_delete_trigger
-        , $task_detail_trigger
-        , $task_detail = $('.task-detail')
-        , $task_detail_mask = $('.task-detail-mask')
-        , task_list = []
-        , current_index
-        , $update_form
-        , $task_detail_content
-        , $task_detail_content_input
-        ;
+    var $form_add_task = $('.add-task'),
+        $window = $(window),
+        $body = $('body'),
+        $task_delete_trigger, $task_detail_trigger, $task_detail = $('.task-detail'),
+        $task_detail_mask = $('.task-detail-mask'),
+        task_list = [],
+        current_index, $update_form, $task_detail_content, $task_detail_content_input, $checkbox_complete, $msg = $('.msg'),
+        $msg_content = $msg.find('.msg-content'),
+        $msg_confirm = $msg.find('.confirmed'),
+        $alerter = $('.alerter');
 
     init();
 
     $form_add_task.on('submit', on_add_task_form_submit);
     $task_detail_mask.on('click', hide_task_detail)
 
+    function pop(arg) {
+        if (!arg) {
+            console.error('pop title is required')
+        }
+
+        var conf = {},
+            $box, $mask, $title, $content, $confirm, $cancel, dfd, timer, confirmed;
+
+        dfd = $.Deferred();
+
+        if (typeof arg == 'string')
+            conf.title = arg;
+        else {
+            conf = $.extent(conf, arg);
+        }
+
+        $box = $('<div>' +
+                '<div class="pop-title">' + conf.title + '</div>' +
+                '<div class="pop-content">' +
+                '<div>' +
+                '<button style="margin-right: 5px;" class="primary confirm">确定</button>' +
+                '<button class="cancel">取消</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>')
+            .css({
+                color: '#444',
+                position: 'fixed',
+                width: 300,
+                height: 'auto',
+                padding: '15px 10px',
+                background: '#fff',
+                borderRadius: 3,
+                boxShadow: '0 1px 2px raba(0, 0, 0, .5)'
+            })
+
+        $title = $box.find('.pop-title').css({
+            padding: '5px 10px',
+            fontWeight: 900,
+            fontSize: 20,
+            textAlign: 'center',
+        })
+
+        $content = $box.find('.pop-content').css({
+            padding: '5px 10px',
+            textAlign: 'center',
+        });
+
+        $confirm = $content.find('button.confirm');
+        $cancel = $content.find('button.cancel');
+
+        function dismiss_pop() {
+            $mask.remove();
+            $box.remove();
+        }
+
+        function adjust_box_position() {
+            var window_width = $window.width(),
+                window_height = $window.height(),
+                box_width = $box.width(),
+                box_height = $box.height(),
+                move_x, move_y;
+
+            move_x = (window_width - box_width) / 2;
+            move_y = (window_height - box_height) / 2 - 20;
+
+            $box.css({
+                left: move_x,
+                top: move_y
+            })
+
+        };
+
+        $window.on('resize', function () {
+            adjust_box_position();
+        });
+
+        $mask = $('<div></div>')
+            .css({
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                background: 'rgba(0, 0, 0, .5)'
+            })
+
+        timer = setInterval(function () {
+            if (confirmed !== undefined) {
+                dfd.resolve(confirmed);
+                clearInterval(timer);
+                dismiss_pop();
+            }
+        }, 50);
+
+        $confirm.on('click', on_confirmed);
+        $cancel.on('click', on_cancel);
+        $mask.on('click', on_cancel);
+
+        function on_cancel() {
+            confirmed = false;
+        }
+
+        function on_confirmed() {
+            confirmed = true;
+        }
+
+        $mask.appendTo($body);
+        $box.appendTo($body);
+        $window.resize();
+        return dfd.promise();
+    };
+
+    function listen_msg_event() {
+        $msg_confirm.on('click', function () {
+            hide_msg();
+        })
+    }
+
     function on_add_task_form_submit(e) {
-        var new_task = {}, $input;
+        var new_task = {},
+            $input;
         // 禁用默认行为
         e.preventDefault();
         $input = $(this).find('input[name=content]');
@@ -32,7 +152,7 @@
 
     function listen_task_detail() {
         var index;
-        $('.task-item').on('dblclick', function() {
+        $('.task-item').on('dblclick', function () {
             index = $(this).data('index')
             show_task_detail(index);
         })
@@ -44,6 +164,28 @@
             show_task_detail(index);
         })
     };
+    // 监听完成task任务事件
+    function listen_checkbox_complete() {
+        $checkbox_complete.on('click', function () {
+            var $this = $(this);
+            // var is_complete = $this.is(':checked');
+            var index = $this.parent().parent().data('index');
+            var item = get(index);
+            if (item.complete) {
+                update_task(index, {
+                    complete: false
+                });
+            } else {
+                update_task(index, {
+                    complete: true
+                });
+            }
+        })
+    }
+
+    function get(index) {
+        return store.get('task_list')[index];
+    }
 
     // 查看task详情
     function show_task_detail(index) {
@@ -60,7 +202,7 @@
     function update_task(index, data) {
         if (index === undefined || !task_list[index])
             return;
-        task_list[index] = data;
+        task_list[index] = $.extend({}, task_list[index], data);
         refresh_task_list();
     }
     // 隐藏task详情
@@ -87,19 +229,25 @@
             '</div>' +
             '</div>' +
             '<div class="remind input-item">' +
-            '<input name="remind_date" type="date" value="' + item.remind_date + '">' +
+            '<label>提醒时间</label>' +
+            '<input class="datetime" name="remind_date" type="text" value="' + (item.remind_date || '') + '">' +
             '</div>' +
             '<div class="input-item"><button type="submit">更新</button></div>' +
             '</form>';
+
+        // 用新模板替换旧模板
         $task_detail.html(tpl);
+        $('.datetime').datetimepicker();
+
+        // 选中其中的form元素
         $update_form = $task_detail.find('form');
         $task_detail_content = $update_form.find('.content');
         $task_detail_content_input = $update_form.find('[name=content]');
-        $task_detail_content.on('dblclick', function() {
+        $task_detail_content.on('dblclick', function () {
             $task_detail_content.hide();
             $task_detail_content_input.show();
         })
-        $update_form.on('submit', function(e) {
+        $update_form.on('submit', function (e) {
             e.preventDefault();
             var data = {};
             data.content = $(this).find('[name=content]').val();
@@ -115,11 +263,12 @@
             var $this = $(this);
             var $item = $this.parent().parent();
             var index = $item.data('index');
-            var tmp = confirm('确定删除？')
-            tmp
-                ? delete_task(index)
-                : null;
-
+            pop('确定删除？')
+                .then(function (r) {
+                    r
+                        ? delete_task(index) 
+                        : null;
+                })
         })
     };
 
@@ -144,24 +293,71 @@
 
     function init() {
         task_list = store.get('task_list') || [];
+        listen_msg_event();
         if (task_list.length) {
             render_task_list();
         }
+        task_remind_check();
+    }
+
+    function task_remind_check() {
+        var current_timestamp;
+        var itl = setInterval(function () {
+            for (var i = 0; i < task_list.length; i++) {
+                var item = get(i),
+                    task_timestamp;
+                if (!item || !item.remind_date || item.informed)
+                    continue;
+                current_timestamp = (new Date()).getTime();
+                task_timestamp = (new Date(item.remind_date)).getTime();
+                if (current_timestamp - task_timestamp >= 1) {
+                    update_task(i, {
+                        informed: true
+                    });
+                    show_msg(item.content);
+                }
+            }
+        }, 300);
+    }
+
+    function show_msg(msg) {
+        if (!msg) return;
+        $msg_content.html(msg);
+        $alerter.get(0).play();
+        $msg.show()
+    }
+
+    function hide_msg(msg) {
+        $msg.hide();
     }
 
     // 渲染全部模板
     function render_task_list() {
         var $task_list = $('.task-list');
         $task_list.html('');
+        var complete_items = [];
         for (var i = 0; i < task_list.length; i++) {
-            var $task = render_task_item(task_list[i], i);
+            var item = task_list[i];
+            if (item && item.complete)
+                complete_items[i] = item;
+            else
+                var $task = render_task_item(item, i);
             $task_list.prepend($task);
+        }
+
+        for (var j = 0; j < complete_items.length; j++) {
+            var $task = render_task_item(complete_items[j], j);
+            if (!$task) continue;
+            $task.addClass('completed');
+            $task_list.append($task);
         }
 
         $task_delete_trigger = $('.action.delete');
         $task_detail_trigger = $('.action.detail');
+        $checkbox_complete = $('.task-list .complete');
         listen_task_delete();
         listen_task_detail();
+        listen_checkbox_complete();
     }
 
     // 渲染单个模板
@@ -169,7 +365,7 @@
         if (!data || !index) return;
         var list_item_tpl =
             '<div class="task-item" data-index="' + index + '">' +
-            '<span><input type="checkbox"></span>' +
+            '<span><input class="complete"' + (data.complete ? 'checked' : '') + ' type="checkbox"></span>' +
             '<span class="task-content">' + data.content + '</span>' +
             '<span class="fr">' +
             '<span class="action delete"> 删除</span>' +
